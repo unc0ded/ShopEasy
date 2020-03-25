66aa03aebea2fb2999626a2a19cb2640c13fbe6b
ClunkyBot <hershkumar01@gmail.com>
2020-03-26 01:28:04 +0530

    Some UI fixes + Add Sign Up & Home Activity

---
+++ app/src/main/java/com/unc0ded/shopdeliver/SignUpActivity.java
@@ -0,0 +1,15 @@
+package com.unc0ded.shopdeliver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
