aca7836a30174661c1c8296c7d288a03e49330db
Shriram Joshi <56927348+shriram-joshi@users.noreply.github.com>
2020-03-26 22:16:45 +0530

    -Added IDs to objects in LoginActivity -Created Sign Up pop up dialog box -Created customer and vendor sign up activities -Completed adding objects on customer sign up activity -Still have to add IDs to some objects on customer sign up activity

---
+++ app/src/main/java/com/unc0ded/shopdeliver/signUpActivities/vendorSignUpActivity.java
@@ -0,0 +1,16 @@
+package com.unc0ded.shopdeliver.signUpActivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.unc0ded.shopdeliver.R;
+
+public class vendorSignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vendor_sign_up);
+
+    }
+}
