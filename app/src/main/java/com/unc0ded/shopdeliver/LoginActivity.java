74c20c18574352e27714355afdc49c2510cf71c6
Hersh Kumar <56927116+unc0ded@users.noreply.github.com>
2020-03-27 00:35:18 +0530

    Revert "Some UI fixes + Add Sign Up & Home Activity"

--- app/src/main/java/com/unc0ded/shopdeliver/LoginActivity.java
+++ app/src/main/java/com/unc0ded/shopdeliver/LoginActivity.java
@@ -3,30 +3,17 @@ package com.unc0ded.shopdeliver;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
-
-    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.MaterialLight);
         setContentView(R.layout.activity_login);
 
-        login=findViewById(R.id.login_btn);
-        Toolbar myToolbar = findViewById(R.id.login_toolbar);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.login_toolbar);
         setSupportActionBar(myToolbar);
-
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i= new Intent(LoginActivity.this,SignUpActivity.class);
-                startActivity(i);
-            }
-        });
     }
 }
