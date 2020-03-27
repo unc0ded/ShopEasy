0d755487eac1c2f84236811eaf4c37220cb4b953
ClunkyBot <hershkumar01@gmail.com>
2020-03-28 01:47:54 +0530

    Misc Fixes

--- app/src/main/java/com/unc0ded/shopdeliver/signUpActivities/customerSignUpActivity.java
+++ app/src/main/java/com/unc0ded/shopdeliver/signUpActivities/customerSignUpActivity.java
@@ -1,9 +1,16 @@
 package com.unc0ded.shopdeliver.signUpActivities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
@@ -14,8 +21,11 @@ import java.util.Objects;
 
 public class customerSignUpActivity extends AppCompatActivity {
 
-    MaterialButton backBtn,signUp;
+    MaterialButton signUp,getOTP,validateOTP;
     TextInputEditText passwordE,reEnterPasswordE;
+    TextInputLayout passField,reEnterPassField;
+    Toolbar simpleBar;
+    AutoCompleteTextView buildingSelect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +33,35 @@ public class customerSignUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_customer_sign_up);
 
         attachID();
+        setSupportActionBar(simpleBar);
 
-        backBtn.setOnClickListener(new View.OnClickListener() {
+        //adding back button to toolbar
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+
+        //ArrayAdapter for building selection
+        String[] BUILDINGS = new String[] {"A", "B", "C", "D"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(customerSignUpActivity.this, R.layout.dropdown_item, BUILDINGS);
+        buildingSelect.setAdapter(adapter);
+
+        getOTP.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                onBackPressed();
+            public void onClick(View view) {
+                Toast.makeText(customerSignUpActivity.this,"OTP sent to mobile number",Toast.LENGTH_SHORT).show();
             }
         });
+
+        validateOTP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                passField.setVisibility(View.VISIBLE);
+                reEnterPassField.setVisibility(View.VISIBLE);
+                signUp.setVisibility(View.VISIBLE);
+            }
+        });
+
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -41,10 +73,24 @@ public class customerSignUpActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // handle arrow click here
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     private void attachID() {
         passwordE = findViewById(R.id.customer_sign_up_password);
         reEnterPasswordE = findViewById(R.id.customer_sign_up_reenter_password);
-        backBtn = findViewById(R.id.customer_sign_up_view_back_btn);
         signUp = findViewById(R.id.customer_sign_up_btn);
+        getOTP = findViewById(R.id.otp_btn);
+        validateOTP = findViewById(R.id.customer_validate_otp);
+        simpleBar=findViewById(R.id.back_toolbar);
+        passField=findViewById(R.id.textInputLayout3);
+        reEnterPassField=findViewById(R.id.textInputLayout2);
+        buildingSelect = findViewById(R.id.dropdown_base);
     }
 }
